c3ae2a31b73afec09b4298f7eeaba6edfd15d11f
dharshini0801 <dharshinisudha0801@gmail.com>
2025-09-19 06:49:29 +0530

    Added arrowfunc folder with programs: greater-than-10, squares, odd-squares

---
+++ arrowfunc/greater-than-10.js
@@ -0,0 +1,7 @@
+const numbers = [3, 7, 12, 18, 5, 25];
+
+const greaterThanTen = numbers.filter((num) => {
+    return num > 10;
+});
+
+console.log(greaterThanTen);
---
+++ arrowfunc/squares.js
@@ -0,0 +1,7 @@
+const numbers = [2, 4, 6, 8];
+
+const squares = numbers.map((num) => {
+    return num * num;
+});
+
+console.log(squares);
