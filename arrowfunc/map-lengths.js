2f49a6bcb47191cc0d4f6b114f61763bc65390b8
dharshini0801 <dharshinisudha0801@gmail.com>
2025-09-19 06:29:28 +0530

    Added arrowfunc folder with three programs

---
+++ arrowfunc/map-lengths.js
@@ -0,0 +1,7 @@
+const words = ["appless", "banana", "grape", "orange"];
+
+const lengths = words.map((word) => {
+    return word.length;
+});
+
+console.log(lengths);
---
+++ arrowfunc/traditional-arrow.js
@@ -0,0 +1,8 @@
+function add(a, b) {
+    return a + b;
+}
+
+const addArrow = (a, b) => {
+    return a + b;
+};
+console.log("Arrow:", addArrow(5, 3));
